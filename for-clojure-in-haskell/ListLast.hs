7f12f490fa0e175c719b76cb643346b6be077588
Karel <improve-your-code@googlegroups.com>
2012-01-09 09:08:32 +1100

    Added not null restriction to listLast property

--- for-clojure-in-haskell/ListLast.hs
+++ for-clojure-in-haskell/ListLast.hs
@@ -1,3 +1,5 @@
+import Test.QuickCheck
+
 listLast [] = error "Cannot determine last element for an empty list"
 listLast [x] = x
 listLast (x:xs) = listLast xs
@@ -9,6 +11,12 @@ listLast' (x:xs) = listLast' xs
 
 -- Idempotency property (sort of as listLast return an element and not a list)
 prop_listLast xs = listLast [listLast xs] == listLast xs
+
+prop_listLastNotNull xs = not (null xs) ==> listLast [listLast xs] == listLast xs
+
+-- Appending the list withouth the last element to the list containing the last element should equal the list.
+-- init [1,2,3] = [1,2]
+prop_listLastConcat xs = not (null xs) ==> (init xs) ++ [listLast xs] == xs
 
 -- Repeating listLast' to the list of the result of applying listLast' to a list should equal to applying listLast' to the same list
 prop_listLast' xs = ((listLast' xs) >>= (\x -> listLast' [x])) == listLast' xs
