a41dc9d71dc20f569c81b663bfb2afeeb0b7befb
Karel <improve-your-code@googlegroups.com>
2012-01-08 21:29:00 +1100

    Implemented QuickCheck properties for listLast function

--- for-clojure-in-haskell/ListLast.hs
+++ for-clojure-in-haskell/ListLast.hs
@@ -1,3 +1,17 @@
 listLast [] = error "Cannot determine last element for an empty list"
 listLast [x] = x
 listLast (x:xs) = listLast xs
+
+listLast' :: [a] -> Maybe a
+listLast' [] = Nothing
+listLast' [x] = Just x
+listLast' (x:xs) = listLast' xs
+
+-- Idempotency property (sort of as listLast return an element and not a list)
+prop_listLast xs = listLast [listLast xs] == listLast xs
+
+-- Repeating listLast' to the list of the result of applying listLast' to a list should equal to applying listLast' to the same list
+prop_listLast' xs = ((listLast' xs) >>= (\x -> listLast' [x])) == listLast' xs
+
+-- The initial elements concatenated with the last element should equal the entire list
+prop_concatLast' xs = ((listLast' xs) >>= (\x -> (Just ((init xs) ++ [x])) )) == Just xs
