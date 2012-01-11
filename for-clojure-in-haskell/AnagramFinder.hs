a30a3c432f30b84e0bc3061735a28bb47f4704a5
Karel <improve-your-code@googlegroups.com>
2012-01-11 21:53:06 +1100

    Implemented AnagramFinder

--- for-clojure-in-haskell/AnagramFinder.hs
+++ for-clojure-in-haskell/AnagramFinder.hs
@@ -1,13 +1,15 @@
-anagramFinder :: [String] -> [[String]]
-anagramFinder xs = anagramFinder' xs []
+import Data.List
 
-anagramFinder' :: [String] -> [[String]] -> [[String]]
-anagramFinder' [] _ = [[]]
-anagramFinder' (x:xs) anagramsMap = let newAnagramsMap = updateAnagrams x anagramsMap
-                                    in anagramFinder' xs newAnagramsMap
+anagramFinder [] = []
+anagramFinder xs = filter (\l -> (length l) > 1) $ map snd $  anagramFinder' xs []
 
-updateAnagrams :: String -> [[String]] -> [[String]]
-updateAnagrams anagram [] = [(sort anagram,[])]
-updateAnagrams anagram ((sortedAnagram, anagrams):rest) | (sort anagram) == sortedAnagram = ((sortedAnagram, addIfNotPresent anagram anagrams):rest)
-                                                        | otherwise = updateAnagrams anagram rest 
-                                                                      
+anagramFinder' [] anagrams = anagrams
+anagramFinder' (x:xs) anagrams = let anagrams' = addOrUpdate x anagrams anagrams
+                                 in anagramFinder' xs anagrams'
+
+addOrUpdate anAnagram [] anagramsAcc = ((sort anAnagram),[anAnagram]):anagramsAcc
+addOrUpdate anAnagram ((anagram, anagrams):rest) anagramsAcc
+    | ((sort anAnagram) == anagram) = if not $ anAnagram `elem` anagrams
+                                        then (anagram, (anAnagram:anagrams)):rest
+                                        else ((anagram, anagrams):rest)
+    | otherwise = addOrUpdate anAnagram rest anagramsAcc
