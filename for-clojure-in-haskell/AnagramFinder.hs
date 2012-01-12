7ae923fae0a5250373fb3d87bec0405cd3cf7845
Karel <improve-your-code@googlegroups.com>
2012-01-12 21:17:04 +1100

    implemented anagram finder

--- for-clojure-in-haskell/AnagramFinder.hs
+++ for-clojure-in-haskell/AnagramFinder.hs
@@ -1,15 +1,13 @@
-import Data.List
+import Data.List as L
+import Data.Map as M
 
-anagramFinder [] = []
-anagramFinder xs = filter (\l -> (length l) > 1) $ map snd $  anagramFinder' xs []
+type Anagrams = [String]
 
-anagramFinder' [] anagrams = anagrams
-anagramFinder' (x:xs) anagrams = let anagrams' = addOrUpdate x anagrams anagrams
-                                 in anagramFinder' xs anagrams'
+anagramFinder :: [String] -> [[String]]
+anagramFinder xs = L.filter (\l -> (length l) > 1) $ M.elems $ foldr (\word anagramsMap -> addOrUpdate word anagramsMap) M.empty  xs
 
-addOrUpdate anAnagram [] anagramsAcc = ((sort anAnagram),[anAnagram]):anagramsAcc
-addOrUpdate anAnagram ((anagram, anagrams):rest) anagramsAcc
-    | ((sort anAnagram) == anagram) = if not $ anAnagram `elem` anagrams
-                                        then (anagram, (anAnagram:anagrams)):rest
-                                        else ((anagram, anagrams):rest)
-    | otherwise = addOrUpdate anAnagram rest anagramsAcc
+addOrUpdate :: String -> Map String Anagrams -> Map String Anagrams
+addOrUpdate word anagramsMap = case M.lookup sortedWord anagramsMap of
+                                 Just anagrams -> M.insertWith (++) sortedWord [word] anagramsMap
+                                 Nothing -> M.insert sortedWord [word] anagramsMap
+                               where sortedWord = sort word
